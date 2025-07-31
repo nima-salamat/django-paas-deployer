ff6bb2472d34e049b4b09418fbe4dcf78635f0cf
nima <ssu87556@gmail.com>
2025-07-31 22:15:19 +0330

    refactor delete default Group django model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import User, Profile, Rule
+from django.contrib.auth.models import Group
+
+@admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
