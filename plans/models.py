963d93f86d8cbbc0d760b6cd1ed9a97adea394c1
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-01 23:48:01 +0330

    choice field for platforms

--- plans/models.py
+++ plans/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
-from users.models import User
 from uuid import uuid4
 
 class Plan(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(_("Name"), max_length=50)
+    platform = models.CharField(_("Platform"), max_length=20, choices=getattr(settings, "TECH_STACK_CHOICES", []))
     max_cpu = models.IntegerField(_("Maximum CPU (vCPU)"))
     max_ram = models.IntegerField(_("Maximum RAM (MB)"))
     max_storage = models.IntegerField(_("Maximum Storage (GB)"))
--- plans/serializers.py
+++ plans/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
-from .models import Plan, PrivateNetwork
+from .models import Plan
 
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
-        fields = [
-            "id",
-            "name",
-            "user",
-            "max_apps",
-            "max_cpu",
-            "max_ram",
-            "max_storage",
-            "created_at",
-            "updated_at",
-        ]
+        fields = "__all__"
         read_only_fields = ["id", "user", "created_at", "updated_at"]
