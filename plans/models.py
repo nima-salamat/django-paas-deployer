71888bbf7c19a385aee09443994f07c39f190d96
nima <ssu87556@gmail.com>
2025-08-01 22:58:07 +0330

    fix Plans models user=models.ForeignKey

--- plans/models.py
+++ plans/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 class Plan(models.Model):
     name = models.CharField(_("Name"), max_length=50)
-    user = models.models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
     max_apps = models.IntegerField(_("Maximum Applications"), default=getattr(settings, "DEFAULT_MAX_APPS"))
     max_cpu = models.IntegerField(_("Maximum CPU (vCPU)"))
     max_ram = models.IntegerField(_("Maximum RAM (MB)"))
