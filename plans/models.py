b8cc251dbe4f2fdf4a0f1d713ee89b6cd9ccdc81
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-01 22:38:03 +0330

    id added to the plans model

--- plans/models.py
+++ plans/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from users.models import User
-
+from uuid import uuid4
 
 class Plan(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(_("Name"), max_length=50)
     user = models.models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
     max_apps = models.IntegerField(_("Maximum Applications"), default=getattr(settings, "DEFAULT_MAX_APPS"))
