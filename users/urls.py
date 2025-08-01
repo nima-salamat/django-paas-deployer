db5fb81d9f39c4b92e9581a50cd2f78c02c88703
nima <ssu87556@gmail.com>
2025-08-01 22:22:48 +0330

    add PasswordViewSet urls set & delete

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .apis import UserAPIView, ProfileViewSet
+from .apis import UserAPIView, ProfileViewSet, PasswordViewSet
 
 urlpatterns = [
     path("api/user/", UserAPIView.as_view(), name="user_api"),
@@ -7,4 +7,6 @@ urlpatterns = [
     path("api/profile/order/", ProfileViewSet.as_view({"post":"order"}), name="profile_order"),
     path("api/profile/delete/", ProfileViewSet.as_view({"post":"delete"}), name="profile_order"),
     path("api/profile/set/", ProfileViewSet.as_view({"post":"set"}), name="profile_order"),
+    path("api/password/set/", PasswordViewSet.as_view({"post", "set"}), name="set_password"),
+    path("api/password/delete/", PasswordViewSet.as_view({"post", "delete"}), name="delete_password"),
 ]
