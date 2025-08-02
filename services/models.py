fabfb175b85c41a84dcb3d1db21fa0891bbb8873
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-02 10:31:32 +0330

    created_at and updated_at added

--- services/models.py
+++ services/models.py
@@ -10,7 +10,9 @@ class PrivateNetwork(models.Model):
     name = models.CharField(_("Name"), max_length=50)
     enabled = models.BooleanField(_("Enabled"), default=False)
     description = models.TextField(_("Description"), blank=True)
-
+    created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated At"), auto_now=True)
+    
     class Meta:
         verbose_name = _("Private Network")
         verbose_name_plural = _("Private Networks")
