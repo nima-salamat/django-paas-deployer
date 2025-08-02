9583384d573e5c91888ff6573710da6b1c046608
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-02 22:23:22 +0330

    implementing the admin

--- deploy/admin.py
+++ deploy/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Deploy
 
-# Register your models here.
+@admin.register(Deploy)
+class DeployAdmin(admin.ModelAdmin):
+    list_display = ("name", "service", "version", "running", "started_at")
+    list_filter = ("running", "service", "started_at")
+    search_fields = ("name", "service__name")
+    readonly_fields = ("started_at",)
+    fieldsets = (
+        (None, {
+            "fields": ("name", "service", "version", "zip_file", "config", "running", "started_at")
+        }),
+    )
