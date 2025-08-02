2228c2168c3b3837d36792b84bfea681d0581ca2
nima <ssu87556@gmail.com>
2025-08-02 23:42:09 +0330

    delete zip file after deploy model deletion

---
+++ deploy/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from .models import Deploy
+import os
+
+@receiver(signal=[post_delete], sender=[Deploy])
+def delete_file_after_delete(sender, instance, **kwargs):
+    if instance.zip_file and instance.zip_file.path:
+        if os.path.isfile(instance.zip_file.path):
+            os.remove(instance.zip_file.path)
+    
