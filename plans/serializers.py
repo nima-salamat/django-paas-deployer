e74616efc44e71308dbd41db0560957131fea1f2
nima <ssu87556@gmail.com>
2025-08-03 02:29:33 +0330

    add UnauthorizedPlanSerializer plans app serializers

--- plans/serializers.py
+++ plans/serializers.py
@@ -5,4 +5,14 @@ class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
         fields = "__all__"
-        read_only_fields = ["id", "user", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+
+class UnauthorizedPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = [
+            "name", "platform", "max_cpu", "max_ram",
+            "max_storage","storage_type", "plan_type",
+            "price_per_hour", "price_per_day","price_per_hour"
+        ]
