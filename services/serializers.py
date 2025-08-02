deae3a11a8a8880d9a86bac9a6a66d2926e3b673
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-02 10:40:34 +0330

    service/serializers.py implemented.

--- services/serializers.py
+++ services/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
-
+from .models import PrivateNetwork, Service
 
 class PrivateNetworkSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = PrivateNetwork
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "updated_at"]
