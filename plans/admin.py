269c8ea11e8ce3b1b75f460842bba9c1eb8808e5
nima <ssu87556@gmail.com>
2025-08-02 20:01:23 +0330

    change Plans model(add plan_type, storage_type & add choices to name filed) update models with new fields

--- plans/admin.py
+++ plans/admin.py
@@ -7,28 +7,40 @@ class PlanAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "name",
+        "platform",
+        "plan_type",
         "max_cpu",
         "max_ram",
         "max_storage",
+        "storage_type",
+        "formatted_price",
         "created_at",
         "updated_at",
     )
     list_display_links = ("id", "name")
-    search_fields = ("name",)
+    search_fields = ("name", "platform")
     ordering = ("name",)
-    list_filter = ("max_cpu", "max_ram", "max_storage")
+    list_filter = ("plan_type", "storage_type", "platform", "name")
     list_per_page = 25
 
     readonly_fields = ("created_at", "updated_at")
+
+    def formatted_price(self, obj):
+        return f"{obj.price:,.0f} Toman"
+    formatted_price.short_description = "Price"
+    formatted_price.admin_order_field = "price"
 
     fieldsets = (
         (None, {
             "fields": (
                 "name",
-                "max_apps",
+                "platform",
+                "plan_type",
+                "storage_type",
                 "max_cpu",
                 "max_ram",
                 "max_storage",
+                "price",
             )
         }),
         ("Timestamps", {
@@ -36,4 +48,3 @@ class PlanAdmin(admin.ModelAdmin):
             "classes": ("collapse",),
         }),
     )
-
--- plans/models.py
+++ plans/models.py
@@ -4,12 +4,32 @@ from django.conf import settings
 from uuid import uuid4
 from config.BaseModel import BaseModel
 
+
+class PlanTypeChoices(models.TextChoices):
+    DB = "DB", _("DB")
+    APP = "APP", _("APP")
+    READY = "READY", _("READY")
+    
+    
+class StorageTypeChoices(models.TextChoices):
+    SSD = "SSD"
+    HDD = "HDD"
+  
+    
+class NameChoices(models.TextChoices):
+    BRONZE = "Bronze", _("Bronze")
+    SILVER = "Silver", _("Silver")
+    GOLD = "Gold", _("Gold")
+    
+    
 class Plan(BaseModel):
-    name = models.CharField(_("Name"), max_length=50)
+    name = models.CharField(_("Name"), max_length=50, choices=NameChoices.choices)
     platform = models.CharField(_("Platform"), max_length=20, choices=getattr(settings, "TECH_STACK_CHOICES", []))
     max_cpu = models.IntegerField(_("Maximum CPU (vCPU)"))
     max_ram = models.IntegerField(_("Maximum RAM (MB)"))
     max_storage = models.IntegerField(_("Maximum Storage (GB)"))
+    storage_type = models.CharField(_("Storage Type"), choices=StorageTypeChoices.choices)
+    plan_type = models.CharField(_("Plan Type"), choices=PlanTypeChoices.choices)
     price = models.DecimalField(
         _("Price"),
         max_digits=10,
@@ -23,5 +43,4 @@ class Plan(BaseModel):
         verbose_name_plural = _("Plans")
 
     def __str__(self):
-        return self.name
-
+        return f"{self.name}, {self.platform}"
