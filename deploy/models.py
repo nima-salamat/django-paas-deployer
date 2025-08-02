41d274583c51a44ae9212a54ee6e6d596fda17e3
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-02 22:23:43 +0330

    some improvements on deploy model

--- deploy/models.py
+++ deploy/models.py
@@ -11,14 +11,18 @@ def zip_file_path(instance, filename):
 class Deploy(BaseModel):
     name = models.CharField(verbose_name=_("Name"), max_length=50, unique=True)
     service = models.ForeignKey(Service, verbose_name=_("Service"), on_delete=models.CASCADE)
-    version = models.FloatField(verbose_name=_("Version"), default=0.00)
+    version = models.DecimalField(_("Version"), max_digits=5, decimal_places=2, default=0.00, help_text=_("Deployment version, e.g., 1.0"))
     zip_file = models.FileField(verbose_name=_("ZIP File"), upload_to=zip_file_path, blank=True, null=True)
     config = models.JSONField(verbose_name=_("Configuration"), blank=True, null=True)
     running = models.BooleanField(verbose_name=_("Running"), default=False)
-    started_at = models.DateTimeField(verbose_name=_("Start Time"), blank=True, null=True)
+    started_at = models.DateTimeField(verbose_name=_("Start Time"), blank=True, null=True, editable=False)
 
     MAX_ZIP_SIZE_MB = 10
 
+    class Meta:
+        verbose_name = _("Deployment")
+        verbose_name_plural = _("Deployments")
+    
     def clean(self):
         super().clean()
         if self.zip_file and self.zip_file.size > self.MAX_ZIP_SIZE_MB * 1024 * 1024:
@@ -28,7 +32,13 @@ class Deploy(BaseModel):
 
     def save(self, *args, **kwargs):
         self.full_clean()
+        if self.running and not self.started_at:
+            from django.utils.timezone import now
+            self.started_at = now()
+        if not self.running:
+            self.started_at = None
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} (v{self.version})"
+
