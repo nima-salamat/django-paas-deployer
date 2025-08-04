6d171eecaead962997702cb235cdfe58615591c1
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-04 10:07:00 +0330

    wrong sender fixed

--- deploy/signals.py
+++ deploy/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import Deploy
 import os
 
-@receiver(signal=[post_delete], sender=[Deploy])
+@receiver(signal=[post_delete], sender=Deploy)
 def delete_file_after_delete(sender, instance, **kwargs):
     if instance.zip_file and instance.zip_file.path:
         if os.path.isfile(instance.zip_file.path):
