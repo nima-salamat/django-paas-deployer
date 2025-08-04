e8c796d96cb6b7859c6765c2d7f2672b386a34b4
nima <ssu87556@gmail.com>
2025-08-04 04:17:39 +0330

    Implement PrivateNetworkViewSet for CRUD functionality on PrivateNetwork model

--- services/apis.py
+++ services/apis.py
@@ -60,3 +60,43 @@ class ServiceViewSet(ModelViewSet):
         service = get_object_or_404(self.get_queryset(), pk=pk)
         service.delete()
         return Response({"success": "Service deleted."}, status=status.HTTP_200_OK)
+
+
+class PrivateNetworkViewSet(ModelViewSet):
+    queryset = PrivateNetwork.objects.all()
+    serializer_class = PrivateNetworkSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    pagination_class = ServiceAdminPagination
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return super().get_queryset()
+        return super().get_queryset().filter(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.get_queryset())
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            request.data["user"] = request.user.id
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({"success": "Private Network created."}, status=status.HTTP_201_CREATED)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None, *args, **kwargs):
+        network = get_object_or_404(self.get_queryset(), pk=pk, user=request.user)
+        serializer = self.get_serializer(network, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": "Private Network updated."}, status=status.HTTP_200_OK)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None, *args, **kwargs):
+        network = get_object_or_404(self.get_queryset(), pk=pk, user=request.user)
+        network.delete()
+        return Response({"success": "Private Network deleted."}, status=status.HTTP_200_OK)
--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .apis import ServiceViewSet
+from .apis import ServiceViewSet, PrivateNetworkViewSet
 
 urlpatterns = [
     path('', ServiceViewSet.as_view({"get": "list", "post": "create"}), name='service-list'),
     path('<pk>/', ServiceViewSet.as_view({"patch": "update", "delete": "destroy"}), name='service-detail'),
+    path('networks/', PrivateNetworkViewSet.as_view({"get": "list", "post": "create"}), name='private-network-list'),
+    path('networks/<pk>/', PrivateNetworkViewSet.as_view({"patch": "update", "delete": "destroy"}), name='private-network-detail'),
 ]
