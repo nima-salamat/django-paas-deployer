8949bc8c88bce1d5b69e925ca5824f7ad0666fe9
ArvinNotDev <arveenjafari19@gmail.com>
2025-08-04 11:22:24 +0330

    plans view test

---
+++ plans/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Plan
+
+def plan_list(request):
+    plans = Plan.objects.all()
+    return render(request, 'plans/plan_list.html', {'plans': plans})
