5df2ba42974b4dc2cd2d7687343107313d65060d
nima <ssu87556@gmail.com>
2025-08-04 04:07:26 +0330

    Implement ServiceViewSet for CRUD functionality on Service model

--- services/apis.py
+++ services/apis.py
@@ -1,14 +1,62 @@
 from .models import Service, PrivateNetwork
+from plans.models import Plan
+from django.shortcuts import get_object_or_404
 from .serializers import PrivateNetworkSerializer, ServiceSerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.pagination import PageNumberPagination
 
 
-# class ServiceViewSet(ViewSet):
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAuthenticated]
+class ServiceAdminPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 50
 
-# NOT IMPLEMENTED YET
+
+class ServiceViewSet(ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    pagination_class = ServiceAdminPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_superuser:
+            return queryset
+        return queryset.filter(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.get_queryset())
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            request.data["user"] = request.user.id
+  
+            network_id = request.data.get("network", None)
+            if not network_id and not PrivateNetwork.objects.filter(id=network_id,user=request.user).exists():
+                return Response({"error": "You must create a Private Network first."}, status=status.HTTP_400_BAD_REQUEST)
+            
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": "Service created."}, status=status.HTTP_201_CREATED)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None, *args, **kwargs):
+        service = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(service, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": "Service updated."}, status=status.HTTP_200_OK)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        service = get_object_or_404(self.get_queryset(), pk=pk)
+        service.delete()
+        return Response({"success": "Service deleted."}, status=status.HTTP_200_OK)
--- services/urls.py
+++ services/urls.py
@@ -1,1 +1,7 @@
-urlpatterns=[]
+from django.urls import path
+from .apis import ServiceViewSet
+
+urlpatterns = [
+    path('', ServiceViewSet.as_view({"get": "list", "post": "create"}), name='service-list'),
+    path('<pk>/', ServiceViewSet.as_view({"patch": "update", "delete": "destroy"}), name='service-detail'),
+]
