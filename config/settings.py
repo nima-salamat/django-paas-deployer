d23b775ce214dd2fd1a3862b1e0f01be6aa81288
nima <ssu87556@gmail.com>
2025-08-05 17:51:44 +0330

    Fix config.settings celery settings CELERY_RESULT_BACKEND addr

--- config/settings.py
+++ config/settings.py
@@ -173,10 +173,8 @@ AUTH_USER_MODEL = "users.User"
 
 
 # Celery settings
-CELERY_RESULT_BACKEND = 'django-db'
-
 CELERY_BROKER_URL = 'redis://localhost:6379/0'            
-CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
