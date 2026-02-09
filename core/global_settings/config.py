14c7dc91bc3ea033b86ade748cad862a1bee078d
nima <ssu87556@gmail.com>
2026-02-09 04:25:16 +0330

    update django dockerfile text

--- core/global_settings/config.py
+++ core/global_settings/config.py
@@ -143,8 +143,13 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+       build-essential \
+    && rm -rf /var/lib/apt/lists/*
+    
 COPY requirements.txt /app/
-RUN pip install --upgrade pip setuptools wheel \
+RUN pip install --no-cache-dir --upgrade pip setuptools wheel \
     && pip install --no-cache-dir -r requirements.txt
 
 COPY . /app
