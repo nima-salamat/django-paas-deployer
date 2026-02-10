60210ced3762cfa39dab6830c29a2a4e7182dee9
nima <ssu87556@gmail.com>
2026-02-10 22:42:02 +0330

    add start stop service api

--- services/apis.py
+++ services/apis.py
@@ -1,7 +1,8 @@
+import functools
 from django.core.serializers import serialize
-
+from django.db import transaction
 from .models import Service, PrivateNetwork, Volume
-from plans.models import Plan
+from deploy.models import Deploy
 from django.shortcuts import get_object_or_404
 from .serializers import PrivateNetworkSerializer, ServiceSerializer, VolumeSerializer, GetServiceSerializer
 from rest_framework.viewsets import ViewSet, ModelViewSet
@@ -10,7 +11,12 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from django.utils.translation import gettext_lazy as _
+from deployments.tasks.deploy import deploy as start_service
+from deployments.tasks.deploy import deploy as stop_service
+from core.global_settings.config import SERVICE_STATUS_CHOICES
+
 
 class ServiceAdminPagination(PageNumberPagination):
     page_size = 10
@@ -148,3 +154,117 @@ class VolumeViewSet(ModelViewSet):
         volume = get_object_or_404(self.get_queryset(), pk=pk, user=request.user)
         volume.delete()
         return Response({"success": _("Volume deleted.")}, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def start_service_apiview(request):
+    service_id = request.data.get("service_id", "")
+    
+    try:    
+        with transaction.atomic():
+            service_item = Service.objects.select_for_update().get(
+                id=service_id, 
+                user=request.user
+            )
+            deploy_item = service_item.selected_deploy
+            if deploy_item is None:
+                return Response(
+                    {
+                        "result": "error",
+                        "detail": _("First select a deploy.")
+                    },
+                    status=status.HTTP_409_CONFLICT
+                )
+            
+            if service_item.status in (
+                SERVICE_STATUS_CHOICES.QUEUED, 
+                SERVICE_STATUS_CHOICES.DEPLOYING,
+                SERVICE_STATUS_CHOICES.STOPPING
+            ):
+                return Response(
+                    {
+                        "result": "error",
+                        "detail": _("You can't start service in (queued, deploying, stopping) modes.")
+                    },
+                    status=status.HTTP_409_CONFLICT
+                )
+            
+            service_item.status = SERVICE_STATUS_CHOICES.QUEUED
+            service_item.save()
+            transaction.on_commit(functools.partial(start_service.delay, str(deploy_item.id)))
+            
+    except Service.DoesNotExist:
+        return Response(
+            {
+                "result": "error",
+                "detail": _(f"Service with this ID:{service_id} not found.")
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    return Response(
+        {
+            "result": "success",
+            "detail": _("Service started.")
+        }, 
+        status=status.HTTP_202_ACCEPTED
+    )
+
+@api_view(["POST"])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def stop_service_apiview(request):
+    service_id = request.data.get("service_id", "")
+    
+    try:    
+        with transaction.atomic():
+            service_item = Service.objects.select_for_update().get(
+                id=service_id,
+                user=request.user
+            )
+            deploy_item = service_item.selected_deploy
+            if deploy_item is None:
+                return Response(
+                    {
+                        "result": "error",
+                        "detail": _("First select a deploy.")
+                    },
+                    status=status.HTTP_409_CONFLICT
+                )
+            
+            if service_item.status in (
+                SERVICE_STATUS_CHOICES.QUEUED, 
+                SERVICE_STATUS_CHOICES.DEPLOYING,
+                SERVICE_STATUS_CHOICES.STOPPING
+            ):
+                return Response({
+                    "result": "error",
+                    "detail": _("You can't stop service in (queued, deploying, stopping) modes.")
+                    },
+                    status=status.HTTP_409_CONFLICT 
+                )
+            
+            service_item.status = SERVICE_STATUS_CHOICES.QUEUED
+            service_item.save()
+            transaction.on_commit(functools.partial(stop_service.delay, (deploy_item.id)))
+            
+    except Service.DoesNotExist:
+        return Response(
+            {
+                "result": "error", 
+                "detail": _(f"Service with this ID:{service_id} not found.")
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    return Response(
+        {
+            "result": "success", 
+            "detail": _("Service stopped.")
+        }, 
+        status=status.HTTP_202_ACCEPTED
+    )
+    
+    
--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,19 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .apis import ServiceViewSet, PrivateNetworkViewSet, VolumeViewSet
-
+from services.apis import (
+    ServiceViewSet,
+    PrivateNetworkViewSet,
+    VolumeViewSet,
+    start_service_apiview,
+    stop_service_apiview
+)
 router = DefaultRouter()
 router.register(r'service', ServiceViewSet, basename='service')
 router.register(r'networks', PrivateNetworkViewSet, basename='private-network')
 router.register(r'volume', VolumeViewSet, basename='volume')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("start_service/", start_service_apiview, name="start_service"),
+    path("stop_service/", stop_service_apiview, name="stop_service")
+    
+]
