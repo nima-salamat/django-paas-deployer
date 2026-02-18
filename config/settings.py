e4dfdbd26312634d3524a3a64ccc2a1a2f98b4e0
nima <ssu87556@gmail.com>
2026-02-18 20:38:30 +0330

    Set schedule beat config

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,12 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
-
+CELERY_BEAT_SCHEDULE = {
+    "monitor_services_every_minute": {
+        "task": "deployments.celery.schedules.monitor_services",
+        "schedule": 20.0, 
+    },
+}
 
 CACHES = {
     'default': {
