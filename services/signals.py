0e9c0b171e1d2b07600c17c15f19a5fd74cc5999
nima <ssu87556@gmail.com>
2026-02-19 05:26:19 +0330

    fix services signal delete:correct service(container and image) name

--- services/signals.py
+++ services/signals.py
@@ -6,4 +6,4 @@ from deployments.core.deploy import Deploy as Deployer
 
 @receiver(signal=pre_delete, sender=Service)
 def delete_deploy_before_delete_service(sender, instance, **kwargs):
-    Deployer.remove_all(instance.name)
+    Deployer.remove_all(instance.get_docker_service_name())
