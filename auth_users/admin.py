8fc5b09c54e8403fe5d2f1a0a78b5e74b1b2ba8b
nima <ssu87556@gmail.com>
2026-07-29 00:47:03 +0330

    add AuthCodeAdmin

--- auth_users/admin.py
+++ auth_users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.utils import timezone
 
-# Register your models here.
+from .models import AuthCode
+
+
+@admin.register(AuthCode)
+class AuthCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "code",
+        "created_at",
+        "is_expired_display",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "code",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "is_expired_display",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    @admin.display(
+        boolean=True,
+        description="Expired",
+    )
+    def is_expired_display(self, obj):
+        return obj.is_expired()
