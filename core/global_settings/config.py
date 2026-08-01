74c77a684f007b1bd644c28ee95333222533fdc6
nima <ssu87556@gmail.com>
2026-08-01 19:34:15 +0330

    return mirror docker config

--- core/global_settings/config.py
+++ core/global_settings/config.py
@@ -144,7 +144,7 @@ class SERVICE_STATUS_CHOICES(models.TextChoices):
     STOPPING = "stopping", _("stopping")
 
 
-MIRROR_DOCKER = "docker.io"
+MIRROR_DOCKER = "docker.arvancloud.ir"
 
 
 class Config:
