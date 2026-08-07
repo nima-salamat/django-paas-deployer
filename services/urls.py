90f42fec41242cb87eb6d831091deeff446cecaf
nima <ssu87556@gmail.com>
2026-08-08 00:23:37 +0330

    handle volume service

--- services/urls.py
+++ services/urls.py
@@ -11,8 +11,6 @@ from services.apis import (
     volume_files_apiview,
     volume_download_apiview,
     purge_service_runtime_apiview,
-    delete_container_only_apiview,
-    delete_image_only_apiview,
 )
 router = DefaultRouter()
 router.register(r'service', ServiceViewSet, basename='service')
@@ -23,8 +21,6 @@ urlpatterns = router.urls + [
     path("start_service/", start_service_apiview, name="start_service"),
     path("stop_service/", stop_service_apiview, name="stop_service"),
     path("purge_service_runtime/", purge_service_runtime_apiview, name="purge_service_runtime"),
-    path("delete_container_only/", delete_container_only_apiview, name="delete_container_only"),
-    path("delete_image_only/", delete_image_only_apiview, name="delete_image_only"),
     path("service_status/", service_status_apiview, name="service_status"),
     path("service/<uuid:pk>/logs/", service_logs_apiview, name="service_logs"),
     path("volume/<uuid:pk>/files/", volume_files_apiview, name="volume_files"),
