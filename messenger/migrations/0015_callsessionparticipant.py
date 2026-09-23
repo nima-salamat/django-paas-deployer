230583b5087846d72738b689d6074d6b25979833
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 07:29:21 +0330

    feat calls: add call participant migration

---
+++ messenger/migrations/0015_callsessionparticipant.py
@@ -0,0 +1,56 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("messenger", "0014_participant_draft"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="CallSessionParticipant",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("joined_at", models.DateTimeField(auto_now_add=True)),
+                ("left_at", models.DateTimeField(blank=True, null=True)),
+                (
+                    "call",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="participants",
+                        to="messenger.callsession",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="messenger_call_participations",
+                        to="users.user",
+                    ),
+                ),
+            ],
+            options={
+                "constraints": [
+                    models.UniqueConstraint(
+                        fields=("call", "user"),
+                        name="uniq_user_per_call_session",
+                    )
+                ],
+                "indexes": [
+                    models.Index(fields=("call", "left_at"), name="messenger_ca_call_id_6fbf5a_idx"),
+                    models.Index(fields=("user", "left_at"), name="messenger_ca_user_id_a9f2f8_idx"),
+                ],
+            },
+        ),
+    ]
